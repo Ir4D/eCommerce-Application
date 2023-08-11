386d6de0e1be7ef2b2a37fe0b48addd9e6a0e070
NataliaVozhdaeva <LomzNat@yandex.ru>
2023-08-11 19:23:33 +0300

    feat: add main-about

--- src/components/main.ts
+++ src/components/main.ts
@@ -1,10 +1,13 @@
 import MainHeroView from './main-hero';
 import MainOfferView from './main-offers';
+import MainAboutView from './main-about';
 
 const createCodeTemplate = (): string => {
   const mainHeroView = new MainHeroView().render;
   const mainOfferView = new MainOfferView().render;
-  return `<main>${mainHeroView}${mainOfferView}</main>`;
+  const mainAboutView = new MainAboutView().render;
+
+  return `<main>${mainHeroView}${mainOfferView}${mainAboutView}</main>`;
 };
 
 export default class MainView {
