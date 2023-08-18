2a544fa0ffce4f488227a0222c3459c9df6142ba
Ir4D <iradedova@gmail.com>
2023-08-18 14:45:42 +0300

    fix: fix problem with sign up button listener

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,3 @@ import { signupCreate } from './services/signupCustomer/signupCustomer';
 const app = new App();
 app.init();
 app.runClient();
-
-const submitForm = document.querySelector('.form-button');
-submitForm?.addEventListener('click', signupCreate);
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,9 +1,17 @@
 import SignupTitleView from './signup-title';
 import SignupFormView from './signup-form';
+import { signupCreate } from '../../services/signupCustomer/signupCustomer';
 
 const createCodeTemplate = (): string => {
   const signupTitleView = new SignupTitleView().render;
   const signupFormView = new SignupFormView().render;
+
+  document.addEventListener('click', (event: MouseEvent) => {
+    const target = event.target as HTMLElement;
+    if (target && target.classList.contains('form-button')) {
+      signupCreate();
+    }
+  });
 
   return `${signupTitleView}${signupFormView}`;
 };
