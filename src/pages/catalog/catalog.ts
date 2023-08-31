a2acc4426dc0c580450cc9b183f2fa4e86869422
Maltsau <emeraldwhistler.dm@gmail.com>
2023-08-31 16:12:49 +0300

    feat: catalog sort method add

--- src/pages/catalog/catalog.ts
+++ src/pages/catalog/catalog.ts
@@ -1,7 +1,7 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable max-lines-per-function */
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 import {
-  CategoryPagedQueryResponse,
   ClientResponse,
   ProductProjection,
   ProductProjectionPagedQueryResponse
@@ -11,11 +11,21 @@ import Router from '../../services/router/router';
 import State from '../../services/state';
 import Component from '../../components/abstract/component';
 
+type SortPatternType =
+  | undefined
+  | 'alphabet-dec'
+  | 'alphabet-inc'
+  | 'price-dec'
+  | 'price-inc';
+
 export default class CatalogView extends Component {
   private errorModal: HTMLDialogElement;
   private currentCategory: string;
+  private sortPattern: SortPatternType;
   private cardContainer: HTMLElement;
   private controls: HTMLFormElement;
+  private abcSortArrow: HTMLDivElement;
+  private priceSortArrow: HTMLDivElement;
 
   constructor() {
     super();
@@ -30,6 +40,11 @@ export default class CatalogView extends Component {
       this.errorModal.close();
     });
     this.currentCategory = 'All categories';
+    this.abcSortArrow = document.createElement('div');
+    this.priceSortArrow = document.createElement('div');
+    [this.abcSortArrow, this.priceSortArrow].forEach((arrow) => {
+      arrow.classList.add('catalog-sort-arrow');
+    });
   }
 
   private renderCatalog(): void {
@@ -63,19 +78,69 @@ export default class CatalogView extends Component {
 
     const abcSortButton = document.createElement('button');
     const priceSortButton = document.createElement('button');
-    const sortButtons = [abcSortButton, priceSortButton];
-    sortButtons.forEach((button) => {
+    [abcSortButton, priceSortButton].forEach((button) => {
       button.classList.add('catalog-sort-button');
     });
     abcSortButton.classList.add('abc');
     priceSortButton.classList.add('price');
 
-    this.controls.append(categorySelect);
-    this.controls.append(searchInput);
-    this.controls.append(searchButton);
-    sortButtons.forEach((button) => {
-      this.controls.append(button);
+    const deleteArrow = (arrow: HTMLDivElement): void => {
+      arrow.classList.remove('increase');
+      arrow.classList.remove('decrease');
+    };
+
+    const changeArrow = (
+      arrow: HTMLDivElement,
+      prev: string,
+      curr: string
+    ): void => {
+      arrow.classList.remove(prev);
+      arrow.classList.add(curr);
+    };
+
+    abcSortButton.addEventListener('click', () => {
+      deleteArrow(this.priceSortArrow);
+      if (!this.sortPattern) {
+        this.sortPattern = 'alphabet-dec';
+        this.abcSortArrow.classList.add('decrease');
+        this.fillCardContainer();
+      } else if (this.sortPattern === 'alphabet-inc') {
+        this.sortPattern = 'alphabet-dec';
+        changeArrow(this.abcSortArrow, 'increase', 'decrease');
+        this.fillCardContainer();
+      } else {
+        this.sortPattern = 'alphabet-inc';
+        changeArrow(this.abcSortArrow, 'decrease', 'increase');
+        this.fillCardContainer();
+      }
     });
+
+    priceSortButton.addEventListener('click', () => {
+      deleteArrow(this.abcSortArrow);
+      if (!this.sortPattern) {
+        this.sortPattern = 'price-dec';
+        this.priceSortArrow.classList.add('decrease');
+        this.fillCardContainer();
+      } else if (this.sortPattern === 'price-inc') {
+        this.sortPattern = 'price-dec';
+        changeArrow(this.priceSortArrow, 'increase', 'decrease');
+        this.fillCardContainer();
+      } else {
+        this.sortPattern = 'price-inc';
+        changeArrow(this.priceSortArrow, 'decrease', 'increase');
+        this.fillCardContainer();
+      }
+    });
+
+    this.controls.append(
+      categorySelect,
+      searchInput,
+      searchButton,
+      abcSortButton,
+      this.abcSortArrow,
+      priceSortButton,
+      this.priceSortArrow
+    );
     this.container.append(this.controls);
 
     /* cards container */
@@ -104,14 +169,34 @@ export default class CatalogView extends Component {
     const cardName = document.createElement('h6');
     cardName.innerText = catalogItem.name.en;
 
-    catalogItemCard.append(cardImage);
-    catalogItemCard.append(cardImage, cardName);
+    const cardPrice = document.createElement('h6');
+    if (catalogItem.masterVariant.prices) {
+      cardPrice.innerText = JSON.stringify(
+        Number(catalogItem.masterVariant.prices[0]?.value.centAmount) / 100
+      );
+    }
+    catalogItemCard.append(cardImage, cardName, cardPrice);
     catalogItemLink.append(catalogItemCard);
 
     return catalogItemLink;
   }
 
   private fillCardContainer(searchPattern?: string): void {
+    const sortByName = (
+      catalog: ProductProjection[] | undefined
+    ): ProductProjection[] | undefined => {
+      catalog?.sort((a, b) => {
+        if (a.name.en < b.name.en) {
+          return -1;
+        }
+        if (a.name.en > b.name.en) {
+          return 1;
+        }
+        return 0;
+      });
+      return catalog;
+    };
+
     const catalog = State.catalog?.body.results;
     const categories = State.categories?.body.results;
     this.cardContainer.innerHTML = '';
@@ -120,11 +205,7 @@ export default class CatalogView extends Component {
       categoryMap.set(`${category.name.en}`, `${category.id}`);
     });
     let outputArr: ProductProjection[] | undefined;
-    if (searchPattern) {
-      outputArr = catalog?.filter((item) =>
-        item.name.en.toLowerCase().includes(searchPattern.toLowerCase())
-      );
-    } else if (this.currentCategory === 'All categories') {
+    if (this.currentCategory === 'All categories') {
       outputArr = catalog;
     } else {
       outputArr = catalog?.filter((item) => {
@@ -133,7 +214,58 @@ export default class CatalogView extends Component {
         );
       });
     }
-
+    if (this.sortPattern) {
+      switch (this.sortPattern) {
+        case 'alphabet-dec': {
+          sortByName(outputArr);
+          break;
+        }
+        case 'alphabet-inc': {
+          sortByName(outputArr)?.reverse();
+          break;
+        }
+        case 'price-dec': {
+          const undefinedPriceArr = outputArr?.filter((item) => {
+            return !item.masterVariant.prices?.length;
+          });
+          const definedPriceArr = outputArr?.filter((item) => {
+            return item.masterVariant.prices?.length;
+          });
+          definedPriceArr?.sort(
+            (a, b) =>
+              a.masterVariant.prices![0].value.centAmount -
+              b.masterVariant.prices![0].value.centAmount
+          );
+          outputArr = [...definedPriceArr!, ...undefinedPriceArr!];
+          break;
+        }
+        case 'price-inc': {
+          const undefinedPriceArr = outputArr?.filter((item) => {
+            return !item.masterVariant.prices?.length;
+          });
+          const definedPriceArr = outputArr?.filter((item) => {
+            return item.masterVariant.prices?.length;
+          });
+          definedPriceArr
+            ?.sort(
+              (a, b) =>
+                a.masterVariant.prices![0].value.centAmount -
+                b.masterVariant.prices![0].value.centAmount
+            )
+            .reverse();
+          outputArr = [...definedPriceArr!, ...undefinedPriceArr!];
+          break;
+        }
+        default: {
+          true;
+        }
+      }
+    }
+    if (searchPattern) {
+      outputArr = catalog?.filter((item) =>
+        item.name.en.toLowerCase().includes(searchPattern.toLowerCase())
+      );
+    }
     outputArr?.forEach((catalogItem) => {
       const catalogItemLink = this.renderCatalogItemCard(catalogItem);
       this.cardContainer.append(catalogItemLink);
