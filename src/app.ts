0fbc72a21ecdb576acbc40f484b455f39a73b9ff
NataliaVozhdaeva <LomzNat@yandex.ru>
2023-09-05 00:42:34 +0300

    fix: modal window on small screens and no-scroll problem

--- src/app.ts
+++ src/app.ts
@@ -48,6 +48,5 @@ export default class App {
   private toggleMenu(): void {
     this.menuIcon?.classList.toggle('clicked');
     this.nav?.classList.toggle('show');
-    this.appContainer?.classList.toggle('stop-scroll');
   }
 }
