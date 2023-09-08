e80b82267a0b7f3fb2a73bc8f128749b60eb6e83
Ir4D <iradedova@gmail.com>
2023-09-08 13:10:08 +0300

    refactor: update cart class

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -3,6 +3,9 @@ import { LineItem } from '@commercetools/platform-sdk';
 import Component from '../../components/abstract/component';
 import State from '../../services/state';
 
+const createElem = (className: string, tag = 'div'): HTMLElement =>
+  Object.assign(document.createElement(tag), { className });
+
 export default class CartView extends Component {
   constructor() {
     super();
@@ -10,11 +13,8 @@ export default class CartView extends Component {
   }
 
   private renderCart(): void {
-    const productsContainer = document.createElement('div');
-    productsContainer.classList.add('cart-products-container');
-
-    const productsTitle = document.createElement('div');
-    productsTitle.classList.add('cart-products-title');
+    const productsContainer = createElem('cart-products-container');
+    const productsTitle = createElem('cart-products-title');
     productsTitle.innerHTML = `
       <div>Image</div>
       <div>Name</div>
@@ -36,33 +36,30 @@ export default class CartView extends Component {
 
   private renderCartItem(product: LineItem): HTMLElement {
     console.log(product);
-    const productElem = document.createElement('div');
-    productElem.classList.add('cart-product-item');
-
-    const productImage = document.createElement('div');
-    productImage.classList.add('cart-product-image');
+    const productElem = createElem('cart-product-item');
+    const productImage = createElem('cart-product-image');
     if (product.variant.images) {
       productImage.style.background = `center / contain no-repeat url('${product.variant.images[0].url}') #ffff`;
     }
 
-    const productName = document.createElement('div');
+    const productName = createElem('cart-product-name');
     if (product.productId) {
       productName.innerHTML = product.name.en;
     }
 
-    const productPrice = document.createElement('div');
+    const productPrice = createElem('cart-product-price');
     if (product.price) {
       productPrice.innerHTML = `${product.price.value.currencyCode} ${String(
         (product.price.value.centAmount / 100).toFixed(2)
       )}`;
     }
 
-    const productQuantity = document.createElement('div');
+    const productQuantity = createElem('cart-product-quantity');
     if (product.quantity) {
       productQuantity.innerHTML = `${product.quantity}`;
     }
 
-    const productTotalPrice = document.createElement('div');
+    const productTotalPrice = createElem('cart-product-price_total');
     if (product.totalPrice) {
       productTotalPrice.innerHTML = `${
         product.totalPrice.currencyCode
